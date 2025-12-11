70d2bfd4590fc0c4c5ee0efd29b10da1dd49b861
Sherman-shop <shuguoheng_sgh@163.com>
2025-12-11 22:40:39 +0000

    cartoverview setup with redux

---
+++ src/features/cart/cartSlice.js
@@ -0,0 +1,57 @@
+import { createSlice } from '@reduxjs/toolkit';
+
+const initialState = {
+  cart: [],
+};
+
+const cartSlice = createSlice({
+  name: 'cart',
+  initialState,
+  reducers: {
+    // payload = newItem (object like the one in initialState.cart)
+    addItem(state, action) {
+      state.cart.push(action.payload);
+    },
+
+    // payload = pizzaId (number)
+    deleteItem(state, action) {
+      state.cart = state.cart.filter((item) => item.pizzaId !== action.payload);
+    },
+
+    // payload = pizzaId (number)
+    increaseItemQuantity(state, action) {
+      const item = state.cart.find((item) => item.pizzaId === action.payload);
+      
+      item.quantity++;
+      item.totalPrice = item.quantity * item.unitPrice;
+    },
+
+    // payload = pizzaId (number)
+    decreaseItemQuantity(state, action) {
+      const item = state.cart.find((item) => item.pizzaId === action.payload);
+
+      item.quantity--;
+      item.totalPrice = item.quantity * item.unitPrice;
+    },
+
+    clearCart(state) {
+      state.cart = [];
+    },
+  },
+});
+
+export const {
+  addItem,
+  deleteItem,
+  increaseItemQuantity,
+  decreaseItemQuantity,
+  clearCart,
+} = cartSlice.actions;
+
+export default cartSlice.reducer;
+
+export const getTotalCartQuantity = (state) => 
+    state.cart.cart.reduce((sum, item) => sum + item.quantity, 0)
+
+export const getTotalCartPrice = (state) => 
+    state.cart.cart.reduce((sum, item) => sum + item.totalPrice, 0)
--- src/ui/Button.jsx
+++ src/ui/Button.jsx
@@ -1,7 +1,7 @@
 import { Link } from 'react-router-dom'
 
 
-export default function Button({ children, disabled, to, type}) {
+export default function Button({ children, disabled, to, type, onClick }) {
   const base='bg-yellow-400 font-semibold text-sm text-stone-800  inline-block uppercase tracking-wide rounded-full hover:bg-yellow-300 focus:bg-yellow-300 transition-colors duration-500 disabled:cursor-not-allowed'
   const styles = {
     primary:base + ' py-3 px-4 md:px-6 md:py-4',
@@ -9,7 +9,20 @@ export default function Button({ children, disabled, to, type}) {
     secondary:' font-semibold text-sm text-stone-400 py-2.5 px-4 md:px-6 md:py-3.5 border-2 border-stone-300 inline-block uppercase tracking-wide rounded-full hover:bg-stone-300 focus:bg-stone-300 focus:outline-none hover:text-stone-800 focus:ring-stone-200 transition-colors duration-500 disabled:cursor-not-allowed'
   }
 
-    if(to) return <Link to={to} className={styles[type]}>{children}</Link>
+  if(to) 
+    return (
+      <Link to={to} className={styles[type]}>
+        {children}
+      </Link>
+  );
+
+  if(onClick) 
+     return (
+      <button onClick={onClick} disabled={disabled} className={styles[type]}>
+        {children}
+      </button>
+  )
+
   return (
     <button disabled={disabled} className={styles[type]}>
         {children}
